86eeb136c7353d3bb352e933fd85ca1ddedbd6a5
Ilja Kocken <i.j.kocken@uu.nl>
2020-02-13 16:54:13 +0100

    change append_ref_deltas to parse new isoreader ref gas format

--- R/reference_gas_delta.R
+++ R/reference_gas_delta.R
@@ -4,19 +4,36 @@
 #' @param .did An iso file, resulting from [isoreader::iso_read_dual_inlet()].
 #' @param d13C_PDB_wg \eqn{\delta^{13}C}{δ13C} reference gas value to overwrite.
 #' @param d18O_PDBCO2_wg \eqn{\delta^{18}O}{δ18O} reference gas value to overwrite.
-append_ref_deltas <- function(.data, .did,
+append_ref_deltas <- function(.data, .did = NULL,
                               d13C_PDB_wg = NULL,
                               d18O_PDBCO2_wg = NULL,
                               quiet = default(quiet)) {
-  if (!quiet) {
-    glue("Info: appending reference gas \u03b4 values from {length(.did)} data file(s)") %>%
-      message()
-  }
+  if (!any(class(.data) %in% c("data.frame", "tbl_df", "tbl")))
+    stop(".data must be a data.frame or tibble", call. = FALSE)
 
-  if (is.null(d13C_PDB_wg) & is.null(d18O_PDBCO2_wg)) {
-    out <- .data %>%
-      left_join(get_ref_delta(.did), "file_id")
+  if (!is.null(.did) & !any(c("iso_file_list", "iso_file") %in% class(.did)))
+    stop(".did must be an 'iso_file' or 'iso_file_list'.", call. = FALSE)
+
+  if (!((is.null(.did) & !is.null(d13C_PDB_wg) & !is.null(d18O_PDBCO2_wg)) |
+          (!is.null(.did) & is.null(d13C_PDB_wg) & is.null(d18O_PDBCO2_wg))))
+    stop("Either .did or d13C_PDB_wg and d18O_PDBCO2_wg must be provided.", call. = FALSE)
+
+  if (is.null(d13C_PDB_wg) & is.null(d18O_PDBCO2_wg) & !is.null(.did)) {
+    if (!quiet) {
+      glue("Info: appending reference gas \u03b4 values from {length(.did)} data file(s)") %>%
+        message()
+    }
+    if (!"file_id" %in% colnames(.data)) {
+      stop(".data must contain the column 'file_id'.", call. = FALSE)
+    } else {
+      out <- .data %>%
+        left_join(get_ref_delta(.did), "file_id")
+    }
   } else {
+    if (!quiet) {
+      glue("Info: appending reference gas \u03b4 values: \u03b413C = {d13C_PDB_wg} and \u03b418O = {d18O_PDBCO2_wg}.") %>%
+        message()
+    }
     out <- .data %>%
       mutate(d13C_PDB_wg = d13C_PDB_wg,
              d18O_PDBCO2_wg = d18O_PDBCO2_wg)
@@ -30,7 +47,9 @@ append_ref_deltas <- function(.data, .did,
 #' @param .did An iso file, resulting from [isoreader::iso_read_dual_inlet()].
 #' @export
 get_ref_delta <- function(.did) {
-  isoreader::iso_get_standards_info(.did, quiet = TRUE) %>%
+  isoreader::iso_get_standards(.did, quiet = TRUE) %>%
+    filter(delta_name %in% c("d 13C/12C", "d 18O/16O")) %>%
+    distinct(file_id, delta_name, .keep_all = TRUE) %>%
     pivot_wider(id_cols = .data$file_id, names_from = .data$delta_name, values_from = .data$delta_value) %>%
     select(.data$file_id, d13C_PDB_wg = .data$`d 13C/12C`, d18O_PDBCO2_wg = .data$`d 18O/16O`) %>%
     as_tibble()
