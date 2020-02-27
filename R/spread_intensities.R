42a5dd52f33e5753dff5e9f9f36771c1d4a1cdc6
Ilja Kocken <i.j.kocken@uu.nl>
2020-02-27 09:16:44 +0100

    spread_match updates

--- R/spread_intensities.R
+++ R/spread_intensities.R
@@ -6,10 +6,11 @@
 #'
 #' @param .data A [tibble][tibble::tibble-package] containing mass intensities per cycle.
 #' @param ids Identifying columns that we'll group by.
-#' @param our_cols Columns with data values that need to be reshaped.
+#' @param our_cols Columns with data values that need to be reshaped. Defaults to v44.mV to v54.mV.
 #' @return A [tibble][tibble::tibble-package] with the sample and reference
 #'   gasses side-by-side.
-spread_intensities  <- function(.data, ids = NULL, our_cols = NULL, quiet = default(quiet)) {
+spread_intensities  <- function(.data, ids = NULL, our_cols = NULL,
+                                names_pattern = "v([4-9]{2}).(mV)", quiet = default(quiet)) {
   if (!quiet)
     message("Info: reshaping data into wide format.")
 
@@ -22,7 +23,6 @@ spread_intensities  <- function(.data, ids = NULL, our_cols = NULL, quiet = defa
 
   if (is.null(our_cols)) {
     our_cols <- c("v44.mV", "v45.mV", "v46.mV", "v47.mV", "v48.mV", "v49.mV", "v54.mV")
-                  ## "v44_diff", "v44_drop", "has_drop", "cycle_drop")
   }
 
   out <- .data %>%
@@ -30,26 +30,29 @@ spread_intensities  <- function(.data, ids = NULL, our_cols = NULL, quiet = defa
     # first lengthen it so that each row is one mass / unit / intensity
     pivot_longer(cols = our_cols,
                  names_to = c("mass", "unit"),
-                 names_pattern = "v([4-9]{2}).(mV)") %>%
+                 names_pattern = names_pattern) %>%
     # then widen it so that sample and ref gas are next to each other for each cycle
     pivot_wider(id_cols = c("file_id", "cycle"),
                 names_from = c(.data$type, .data$mass),
                 values_from = .data$value) %>%
     # clean up names
     purrr::set_names(~ str_replace_all(., "standard_", "r") %>%
-                       str_replace_all("sample_", "s"))
+                       str_replace_all("sample_", "s")) %>%
+    ungroup(file_id)
   # NOTE: this is neat, but gets rid of all the extra info in cycle_dis etc.
 
-  # so we add cycle_dis info back, in a very simplified form
+  # so we add cycle_dis info back
   cycle_dis_dfr <- .data %>%
-    ungroup() %>%
     select(-one_of(our_cols)) %>%
     pivot_wider(id_cols = ids,
                 names_from = .data$type,
-                values_from = c(.data$v44_low, .data$v44_high, .data$v44_drop,
-                                .data$drop_before, .data$has_drop)) %>%
-    group_by(file_id)
+                ## NOTE: these are now hardcoded here, and are strictly related
+                ## to what's happening in the internals of find_bad_cycles!!
+                values_from = c(.data$v44_low, .data$v44_high, .data$v44_diff, .data$v44_drop,
+                                .data$has_drop, .data$cycle_drop, .data$drop_before, .data$outlier_cycle)) %>%
+    purrr::set_names( ~ str_replace_all(., "^(v44_)?(.*)_standard", "r44_\\2") %>%
+                        str_replace_all("^(v44_)?(.*)_sample", "s44_\\2"))
 
-  left_join(out, cycle_dis_dfr, by = c("file_id", "cycle")) %>%
+  left_join(out, cycle_dis_dfr, by = ids) %>%
     as_tibble()
 }
--- R/spread_match.R
+++ R/spread_match.R
@@ -1,11 +1,10 @@
 #' Spread and match sample gas and reference gas.
 #'
 # #' @param .data A [tibble][tibble::tibble-package], resulting from [correct_backgrounds()]
-#' @inheritParams spread_intensities
 #' @inheritParams match_intensities
 #' @export
-spread_match <- function(.data, method = "normal", quiet = default(quiet)) {
+spread_match <- function(.data, method = "normal", masses = c(44:49, 54), quiet = default(quiet)) {
   .data %>%
-    spread_intensities(quiet = quiet) %>%
-    match_intensities(method = method, quiet = quiet)
+    spread_intensities(our_cols = paste0("v", masses, ".mV"), quiet = quiet) %>%
+    match_intensities(method = method, masses = masses, quiet = quiet)
 }
