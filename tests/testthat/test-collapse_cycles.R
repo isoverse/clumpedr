d68a69397874791525543ef2648ae90d099e7dd3
Ilja Kocken <i.j.kocken@uu.nl>
2020-02-27 09:17:47 +0100

    add tests for collapse cycles

--- tests/testthat/test-collapse_cycles.R
+++ tests/testthat/test-collapse_cycles.R
@@ -2,10 +2,17 @@ context("Collapsing cycles")
 test_that("collapsing the cycles", {
   collapse_test <- standards %>%
     isoreader::iso_get_raw_data() %>%
-    find_bad_cycles() %>%
+    disable_cycles(genplot = FALSE) %>%
     spread_match() %>%
+    filter(cycle != 0) %>%
+    mutate(outlier_cycle = s44_outlier_cycle | r44_outlier_cycle) %>%
     append_ref_deltas(standards) %>%
     delta_values(genplot=FALSE) %>%
-    collapse_cycles(d45:d49, d18O_PDBCO2, d13C_PDB, D47_raw)
+    collapse_cycles(cols = c(d18O_PDB, d13C_PDB))
   expect_is(collapse_test, "tbl_df")
+  # do the summary columns exist?
+  expect_true(all(expand.grid(c("d18O_PDB", "d13C_PDB"),
+                              c("mean", "sd", "sem", "cl")) %>%
+                    tidyr::unite(col = "new") %>%
+                    pull(new) %in% colnames(collapse_test)))
 })
